4a37e8d4eba355a0e9da659caf94039b9aab6992
cybertanya <30558706+cybertanya@users.noreply.github.com>
2022-02-28 21:45:39 +0200

    Late night update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # I-Am-RIch-App
 l am rich application is an app l am making while learning flutter 
+
+For generating icons for our icon you can use https://www.appicon.co
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text("I Am RIch"),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/unnamed.png'),
+          ),
         ),
       ),
     ),
